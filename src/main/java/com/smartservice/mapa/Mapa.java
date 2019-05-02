45298b31a2588a407bf959f64614c7df500f1642
Jorge Ortega Hernandez <35391140+JorgeOrtegaHdz@users.noreply.github.com>
2019-05-02 07:42:29 -0500

    Add files via upload

--- src/main/java/com/smartservice/mapa/Mapa.java
+++ src/main/java/com/smartservice/mapa/Mapa.java
@@ -12,6 +12,12 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL; 
+import java.lang.StringBuffer;
+
 
 /**
  *
@@ -19,7 +25,8 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet(name = "Mapa", urlPatterns = {"/Mapa"})
 public class Mapa extends HttpServlet {
-
+    double latitude=0;
+    double longitude=0;
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
      * methods.
@@ -33,6 +40,26 @@ public class Mapa extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
+            
+            String url = "https://maps.googleapis.com/maps/api/distancematrix/"+
+                    "json?origins="+latitude+"|"+longitude+"&destinations="+latitude+
+                    "|"+longitude+"&mode=driving&language=fr-FR&key=AIzaSyDOXbhiOlje54s-im1CDxcV-cK0U6OxCB0";
+            URL obj = new URL(url);
+            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+            con.setRequestMethod("GET");
+            con.setRequestProperty("User-Agent", "Mozilla/5.0");
+            int responseCode = con.getResponseCode();
+            System.out.println("\nSending 'GET' request to URL : " + url);
+            System.out.println("Response Code : " + responseCode);
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuffer datos = new StringBuffer();
+            while ((inputLine = in.readLine()) != null) {
+               datos.append(inputLine);
+            }
+            in.close();
+            System.out.println(datos.toString());
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
@@ -40,18 +67,28 @@ public class Mapa extends HttpServlet {
             out.println("<meta charset=\"utf-8\">");
             out.println("<title>Servlet Mapa</title>"); 
             out.println("<script type='text/javascript' src='Mapa.js' ></script>");
-            
+            out.println("<script src=\"http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js\"></script>");
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Mapa</h1>");
+            out.println("<h1>SmartServices</h1>");
+            out.println("<button type=\"button\" onclick=\"main()\" value=\"IniciarAplicacion\">Iniciar</button>");
+            out.println("<iframe\n" +
+                    "      width=\"600\"\n" +
+                    "      height=\"450\"\n" +
+                    "      frameborder=\"0\" style=\"border:0\"\n" +
+                    "      src=\"https://www.google.com/maps/embed/v1/place?key=AIzaSyDOXbhiOlje54s-im1CDxcV-cK0U6OxCB0\n" +
+                    "        &q=Space+Needle,Seattle+WA\" allowfullscreen>\n" +
+                    "    </iframe>");
             out.println("<p>Nueva entrada de Jorge</p>");
             out.println("<h1>Servlet Mapa at " + request.getContextPath() + "</h1>");
             //out.println("<source src='Mapa.js' ></p>");
-            out.println("<div id='map'></div>");
+            out.println("<p id='map'></p>");
             out.println("<div id='error'></div");
             out.println("</body>");
             out.println("</html>");
-        }
+        }/*catch(ClassNotFoundException ex){
+            ex.printStackTrace();
+        }*/
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
--- target/Mapa-1.0-SNAPSHOT/Mapa.js
+++ target/Mapa-1.0-SNAPSHOT/Mapa.js
@@ -1,26 +1,24 @@
 //Script Mapa para Servlet
 
-(function (){
-	var output = document.getElementById("map");
-	if(navigator.geolocation){
-		output.innerHTML="<p>Geolocalizando</p>";
-		var latitude,longitude;
-		latitude,longitude=localizacion();
-		output.innerHTML="<p>Latitud: "+latitude+" Longitude: "+longitude+"</p>";
-	}else{
-		output.innerHTML="Tu navegador es demaciado antiguo, por favor actulizalo";
-	}
-        
-        function localizacion(posicion){
-            var latitude=posicion.coords.latitude;
-            var longitude=position.coords.longitude;
-            return latitude,longitude;
-        }
+function main(){
+  	var x = document.getElementById("map");
+    var latitude, longitude;
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+    } else { 
+      x.innerHTML = "Geolocation is not supported by this browser.";
+    }
 
-        function errorLoc(){
-                var outError=document.getElementById("error");
-                outError.innerHTML("<p>No se pudo obtener tu ubicación</p>");
-        }
-        
-        navigator.geolocation.getCurrentPosition(localizacion,errorLoc);
-}).call(this);
+    function showPosition(position) {
+      latitude=position.coords.latitude;
+      longitude=position.coords.longitude;
+      x.innerHTML = "Latitude: " + position.coords.latitude + 
+      "<br>Longitude: " + position.coords.longitude;
+      var datosJSON="https://maps.googleapis.com/maps/api/distancematrix/json?origins="+latitude+"|"+longitude+"&destinations="+latitude+"|"+longitude+"&mode=driving&language=fr-FR&key=AIzaSyDOXbhiOlje54s-im1CDxcV-cK0U6OxCB0";
+      //var xmlHttp = new XMLHttpRequest(); 
+      //xmlHttp.open("GET", datosJSON, false); // false for synchronous request 
+      //xmlHttp.send(null); 
+      //var resp=xmlHttp.responseText;
+      x.innerHTML="<p>"+datosJSON+"</p>"; 
+    }
+}
